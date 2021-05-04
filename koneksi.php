469ead0ec01ead9d23ab5cc91eccba29797881ed
raniaathalaaa <79816636+raniaathalaaa@users.noreply.github.com>
2021-05-04 07:50:26 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -12,26 +12,4 @@
 
 		return $rows;
 	}
-	function cari($keyword){
-		$query = "SELECT * FROM databook
-			WHERE
-			Nama_Pemilik LIKE '%$keyword%' OR
-			Nama_Anabul LIKE '%$keyword%' OR
-			Jenis_Anabul LIKE '%$keyword%' OR
-			Usia LIKE '%$keyword%' OR
-			Jenis_Pesanan LIKE '%$keyword%' OR
-			Alamat LIKE '%$keyword%' 
-		";
-		return query($query);
-	}
-	function user($keyword){
-		$query = "SELECT * FROM regist
-			WHERE
-			nama_user LIKE '%$keyword%' OR
-			username_user LIKE '%$keyword%' OR
-			email_user LIKE '%$keyword%' OR
-			photo_user LIKE '%$keyword%' 
-		";
-		return query($query);
-	}
   ?>
